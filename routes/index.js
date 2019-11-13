2b0894a4406dbfd9599a9e35b0ed31695cb86f10
Takashi Abe <tabe@tabe-ltm4.internal.salesforce.com>
2019-11-13 10:34:03 +0900

    change pictures for Salesforce DEMO

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Salesforce 女子部 #19' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Salesforce CI/CD サンプル' });
 });
 
 module.exports = router;
